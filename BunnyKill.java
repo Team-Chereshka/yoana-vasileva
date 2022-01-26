77db2d524e7a21b30cc7f9365f7d975e7e6147fb
Yoana <yoni.vasileva8b@gmail.com>
2022-01-26 15:09:17 +0200

    done

--- BunnyKill.java
+++ BunnyKill.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BunnyKill {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int power = 0;
         System.out.println("Enter the layout");
         String matrix = sc.nextLine();
         String[] input = matrix.split(" ");
@@ -25,35 +25,49 @@ public class BunnyKill {
 
         System.out.println("Enter the bomb bunny coordinates");
         String count = sc.nextLine();
-
-
         String[] coordinates = count.split(" ");
 
 
+        int snowball = 0;
         for (int i = 0; i < coordinates.length; i++) {
+
             //razdelqme 1,1 -> 1row, 1col
             String[] splitCoordinates = coordinates[i].split(",");
             int bunnyRow = Integer.parseInt(splitCoordinates[0]);
             int bunnyCol = Integer.parseInt(splitCoordinates[1]);
+            int bunnyPower = layout[bunnyRow][bunnyCol];
+            snowball += bunnyPower;
 
+            for (int j = bunnyRow - 1; j <= bunnyRow + 1; j++) {
+                for (int z = bunnyCol - 1; z <= bunnyCol + 1; z++) {
+                    if(j >= 0 && j < rows && z >= 0 && z < cols) {
+                        layout[j][z] -= bunnyPower;
+                    }
+                }
+            }
         }
 
-
-        List<String> badBunnies = new ArrayList<>();
-        for (int i = 0; i < coordinates.length; i++) {
-            badBunnies.add(coordinates[i]);
+        for (int row = 0; row < layout.length; row++) {
+            for (int col = 0; col < layout.length; col++) {
+                if (layout[row][col] <0){
+                    layout[row][col] = 0;
+                }
+                snowball += layout[row][col];
+            }
         }
-        System.out.println(badBunnies);
 
-
-
-
-       for (int i = 0; i < layout.length; i++) {
-            for (int j = 0; j < layout.length; j++) {
-                System.out.print(layout[i][j] + " ");
+        for (int row = 0; row < layout.length; row++) {
+            for (int col = 0; col < layout.length; col++) {
+                System.out.print(layout[row][col]+ " ");
             }
             System.out.println();
         }
+
+
+        System.out.printf("%d", snowball);
+        System.out.println();
+
+
 
 
     }
